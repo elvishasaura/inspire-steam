0b884b5530347fb0509fb464edc269af61736537
elvishasaura <elvmut2007@gmail.com>
2026-02-11 09:17:01 +0300

    first commit

---
+++ python-basics/Challenge 1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+Grade = "B+"
+favourite_course = "IT" 
+Cluster_points = "67"
+
+print(f"My favourite university is {university}")
+print(f"My HighSchool grade was {Grade}")
+print(f"My favourite course is {favourite_course}")
+print(f"My cluster points were {Cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("hello from Elvis")
+# city/town
+print("I am from Juja")
+# my hobbies
+print("I like to play fortnite, walking and singing")
+
+age = 18
+height = 1.80
+weight = 65
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"And weigh {weight} kgs")
