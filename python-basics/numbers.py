a78c3cd1c34b5db4305d5a46aedc4fd1087e9fdb
elvishasaura <elvmut2007@gmail.com>
2026-02-11 10:39:11 +0300

    input

---
+++ python-basics/numbers.py
@@ -0,0 +1,21 @@
+# Name : Elvis Mutuma
+# Date : 11/02/2026
+
+# Program to add, subtract, divide, multiply, 2 numbers from input
+
+number_one = 50
+number_two = 17
+
+
+sum_numbers = number_one + number_two
+difference_numbers = number_one - number_two
+quotient_numbers = number_one / number_two
+product_numbers = number_one * number_two
+
+
+
+print(f"The sum of the numbers : {sum_numbers}")
+print(f"The difference of the numbers : {difference_numbers}")
+print(f"The quotient of the numbers : {quotient_numbers}")
+print(f"The product of the numbers : {product_numbers}")
+
